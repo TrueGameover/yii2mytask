6b8472aa214cfb7044374cee5bf020d96a3d21a3
Aleksey Ivachev <truegameover@gmail.com>
2017-07-21 01:58:25 +0700

    Изменил текст страницы.

---
+++ views/site/about.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'О компании';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        Эта страница доступна только если вы авторизованный пользователь
+    </p>
+</div>
