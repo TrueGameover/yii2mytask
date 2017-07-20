b28693a6dc6399e9ca3b1fe9051835f5713aa616
Aleksey Ivachev <truegameover@gmail.com>
2017-07-21 01:58:43 +0700

    Изменил текст главной страницы.

---
+++ views/site/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+
+        <h1>Привет!</h1>
+
+        <p class="lead">Это лучшая фирма</p>
+    </div>
+
+</div>
